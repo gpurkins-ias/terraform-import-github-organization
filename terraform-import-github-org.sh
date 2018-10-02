6a00250b7f1b9ba727816c6af92bd7a932112f21
Matthew Maycock <mmaycock@integralads.com>
2018-10-02 11:18:26 -0400

    CICD-1022 Factored out team-permission declaration for loop simplification.

--- terraform-import-github-org.sh
+++ terraform-import-github-org.sh
@@ -89,6 +89,20 @@ EOF
     return 0
 }
 
+declare_team_perms () {
+    local resource="$1"
+    local team_id="$2"
+    local repo="$3"
+    local perms="$4"
+    cat << EOF
+resource "github_team_repository" "${resource}" {
+    team_id    = "${team_id}"
+    repository = "${repo}"
+    permission = "${perms}"
+}
+EOF
+}
+
 
 # Public Repos
   # You can only list 100 items per page, so you can only clone 100 at a time.
@@ -186,6 +200,7 @@ get_team_repos () {
   local team_data="$( curl -s "${API_URL_PREFIX}/teams/${TEAM_ID}?access_token=${GITHUB_TOKEN}" )"
   local team_name="$( jq -r .name <<< "${team_data}" )"
   local team_slug="$( jq -r .slug <<< "${team_data}" )"
+  local perms=
 
   for PAGE in $(limit_team_pagination); do
     for i in $(curl -s "${API_URL_PREFIX}/teams/$TEAM_ID/repos?access_token=$GITHUB_TOKEN&page=$PAGE&per_page=100" | jq -r 'sort_by(.name) | .[] | .name'); do
@@ -196,33 +211,15 @@ get_team_repos () {
     PULL_PERMS=$(curl -s "${API_URL_PREFIX}/teams/$TEAM_ID/repos/$ORG/$i?access_token=$GITHUB_TOKEN" -H "Accept: application/vnd.github.v3.repository+json" | jq -r .permissions.pull )
   
     if [[ "$ADMIN_PERMS" == "true" ]]; then
-      cat >> github-teams-$TEAM_SLUG.tf << EOF
-resource "github_team_repository" "$TEAM_SLUG-$TERRAFORM_TEAM_REPO_NAME" {
-  team_id    = "$TEAM_ID"
-  repository = "$i"
-  permission = "admin"
-}
-
-EOF
+        perms=admin
     elif [[ "$PUSH_PERMS" == "true" ]]; then
-      cat >> github-teams-$TEAM_SLUG.tf << EOF
-resource "github_team_repository" "$TEAM_SLUG-$TERRAFORM_TEAM_REPO_NAME" {
-  team_id    = "$TEAM_ID"
-  repository = "$i"
-  permission = "push"
-}
-
-EOF
+        perms=push
     elif [[ "$PULL_PERMS" == "true" ]]; then
-      cat >> github-teams-$TEAM_SLUG.tf << EOF
-resource "github_team_repository" "$TEAM_SLUG-$TERRAFORM_TEAM_REPO_NAME" {
-  team_id    = "$TEAM_ID"
-  repository = "$i"
-  permission = "pull"
-}
-
-EOF
+        perms=pull
+    else
+        continue
     fi
+    declare_team_perms "${TEAM_SLUG}-${TERRAFORM_TEAM_REPO_NAME}" "${TEAM_ID}" "${i}" "${perms}" >> github-teams-$TEAM_SLUG.tf
     terraform import github_team_repository.$TEAM_SLUG-$TERRAFORM_TEAM_REPO_NAME $TEAM_ID:$i
     done
   done
